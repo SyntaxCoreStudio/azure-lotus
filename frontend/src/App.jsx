7b096d266b344a2c61fa96d6cef42a7890710ec5
SyntaxCoreStudio <cjdonnelly2001@gmail.com>
2026-02-02 17:23:19 +1000

    setup react frontend with tailwind and added base home page (no styling)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,45 @@
+export default function App() {
+  return (
+    <>
+      <header>
+        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.hIEVP2OIafAuPENf6eZyLQHaHa%3Fpid%3DApi&f=1&ipt=84cd47bc1bbcb96fe1b3543317cd1d9c400e31a04a3d706f2525e2802d5b9cd3&ipo=images" />
+        <p>Call Family</p>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">About</a>
+            </li>
+            <li>
+              <a href="/">How it works</a>
+            </li>
+            <li>
+              <a href="/">Sign up</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <div>
+          <h1>Connecting and caring for Seniors</h1>
+          <button>Free Download</button>
+          <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.hIEVP2OIafAuPENf6eZyLQHaHa%3Fpid%3DApi&f=1&ipt=84cd47bc1bbcb96fe1b3543317cd1d9c400e31a04a3d706f2525e2802d5b9cd3&ipo=images" />
+        </div>
+      </main>
+      <footer>
+        <div>
+          <ul>
+            <li>Contact</li>
+            <li>Privacy Policy</li>
+            <li>Terms of use</li>
+          </ul>
+          <p>&copy; 2026</p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+
